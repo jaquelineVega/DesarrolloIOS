b7d7822fb7c8a8b175ebe48c43e715fe98cb7498
jaquelineVega <jaquelin.geo17@gmail.com>
2021-03-08 21:54:20 -0600

    Practica 1

---
+++ Practica1-tiposdedatos/Shared/Practica1_tiposdedatosApp.swift
@@ -0,0 +1,17 @@
+//
+//  Practica1_tiposdedatosApp.swift
+//  Shared
+//
+//  Created by MTWDM on 06/02/21.
+//
+
+import SwiftUI
+
+@main
+struct Practica1_tiposdedatosApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
